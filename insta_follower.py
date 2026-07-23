1a2ed18c53e8f65614f0378000c7a5a651bd4842
Diana Angheluta <dianaangheluta@Dianas-MacBook-Pro.local>
2026-07-23 21:37:06 +0300

    added automated following

--- insta_follower.py
+++ insta_follower.py
@@ -2,9 +2,13 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
+from time import sleep
+
 import os
 from dotenv import load_dotenv
 from selenium import webdriver
+
 
 class InstaFollower:
     def __init__(self):
@@ -32,7 +36,7 @@ class InstaFollower:
 
         cookies_popup = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[class='naan-popup-dismiss']")))
         cookies_popup.click()
-        
+   
 
     def find_followers(self):
         search_button = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/nav/button")))
@@ -40,10 +44,35 @@ class InstaFollower:
 
         make_search = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/aside/div[2]/input")))
         make_search.click()
-        make_search.send_keys(os.getenv("SIMILAR_ACCOUNT") + Keys.ENTER)
+        make_search.send_keys(os.getenv("SIMILAR_ACCOUNT"))
 
-        followers = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/main/header/div[2]/div[2]/span[2]/a")))
-        followers.click()
+        top_result = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/aside/div[4]/a")))
+        top_result.click()
 
-    def follow(self):
-        pass
+        followers_page = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/main/header/div[2]/div[2]/span[2]/a")))
+        followers_page.click()
+        
+        container = self.driver.find_element(By.CSS_SELECTOR, "div[class='followers-scroll _aano']")
+        row = self.driver.find_elements(By.CSS_SELECTOR, ".naan-follower-row")[0]
+        row_height = self.driver.execute_script("return arguments[0].getBoundingClientRect().height", row)
+
+        idx = 0
+
+        while True:
+            all_followers = WebDriverWait(self.driver, 10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "div[class='naan-follower-row']")))
+            print(f"Current Index: {idx}; All followers: {len(all_followers)}")
+            if idx == len(all_followers):
+                break
+            current_follower = all_followers[idx].find_element(By.CSS_SELECTOR, "button")
+            self.follow(current_follower)
+            idx += 1
+            sleep(0.2)
+            self.driver.execute_script(
+                "arguments[0].scrollTop += arguments[1]", container, row_height
+            )
+
+        print("Follower List ended!")
+
+    def follow(self, follower):
+        if follower.text == "Follow":
+            follower.click()
